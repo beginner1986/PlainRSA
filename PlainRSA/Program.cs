a366385f512927a89a79fc785bec4a859ea5d132
Adam <beginner1986@gmail.com>
2019-11-26 14:16:05 +0100

    Add project files.

---
+++ PlainRSA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlainRSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
